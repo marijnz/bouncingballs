34fcf67515d2b732f0d05d28ca323505d765fbe8
Arco Mul <me@arcomul.nl>
2015-01-14 11:04:11 +0100

    New character, a robot!

--- src/data/scripts/main.lua
+++ src/data/scripts/main.lua
@@ -1,10 +1,13 @@
 logMessage("using main.lua")
 
+FLOOR_Z = 0.19
+CEILING_Z = 4.6 
+
 -- Options 
 local options = {
     freecamera = false,
     debugDrawing = false
-	}	
+}	
 
 PhysicsSystem:setDebugDrawingEnabled(options.debugDrawing)
 
@@ -27,6 +30,9 @@ level = GameObjectManager:createGameObject("level")
 level.render = level:createRenderComponent()
 level.render:setPath("data/models/cube-level.thModel")
 level:setPosition(Vec3(0, 0, 0))
+
+-- Some mathematical functions
+include("util.lua")
 
 -- Classes
 include("objectManager.lua")
--- src/data/scripts/player.lua
+++ src/data/scripts/player.lua
@@ -1,27 +1,48 @@
 logMessage("using player.lua")
 
+ROBOT_CAMERA_OFFSET = Vec3(0.5, 0, 0.55);
+
+-- Player/robot model
 player = GameObjectManager:createGameObject("player")
 player.render = player:createRenderComponent()
-player.render:setPath("data/models/player.thModel")
-player:setPosition(Vec3(0, 0, 0))
+player.render:setPath("data/models/robot.thModel")
 
+-- Player rigid body for collision and physics
 player.pc = player:createPhysicsComponent()
 cinfo = RigidBodyCInfo()
-cinfo.shape = PhysicsFactory:createCapsule(Vec3(0.0, 0.0, 0.35), Vec3(0.0, 0.0, 1.05), 0.4)
+cinfo.shape = PhysicsFactory:createBox(Vec3(0.5, 0.5, 0.25))
 cinfo.motionType = MotionType.Dynamic
 cinfo.mass = 1.0
 cinfo.friction = 1.0
 cinfo.angularDamping = 0.0
 cinfo.restitution = 1.0
-cinfo.position = Vec3(0.0, 0.0, 0.0)
+cinfo.position = Vec3(0.0, 0.0, FLOOR_Z + 0.25)
 cinfo.maxAngularVelocity = 0.0
 player.pc:createRigidBody(cinfo)
-
 player.speed = 5
+
+-- Robot camera model
+player.robotCamera = GameObjectManager:createGameObject("robotCamera")
+player.robotCamera.render = player.robotCamera:createRenderComponent()
+player.robotCamera.render:setPath("data/models/robot-camera.thModel")
+player.robotCameraRotation = 0
+player.robotCameraRotationDirection = 1
+player.robotCamera:setPosition(player:getPosition() + ROBOT_CAMERA_OFFSET)
 
 bulletCooldown = 0.2
 
---local bullet = Bullet(player:getPosition() + Vec3(0,0,0))
+-- Rotates the camera of the robot slowly and takes the player rotation in account to
+-- calculate the right rotation
+-- <playerRotation> is a Quaternion
+updateRobotCameraRotation = function (playerRotation) 
+    player.robotCameraRotation = player.robotCameraRotation + (player.robotCameraRotationDirection * 1)
+    if (math.abs(player.robotCameraRotation) > 35) then
+        player.robotCameraRotationDirection = player.robotCameraRotationDirection * -1
+    end
+    local rotation = Quaternion(Vec3(0, 0, 1), player.robotCameraRotation)
+    player.robotCamera:setRotation(rotation * playerRotation)
+end
+
 player.update = function (guid, deltaTime) 	
     -- The direction the player is going to walk this frame
     local direction = Vec3(0.0, 0.0, 0.0)
@@ -37,6 +58,28 @@ player.update = function (guid, deltaTime)
     if (InputHandler:isPressed(Key.Right)) then
         direction = direction + Vec3(-1, 1, 0)
     end
+
+    -- If a direction is set, walk & rotate
+    if (direction.x ~= 0 or direction.y ~= 0) then
+        -- Normalize the direction for when two buttons are pressed at the same time
+        local normalized = direction:normalized()
+
+        -- Multiply speed by the direction to walk
+        player.pc:getRigidBody():setLinearVelocity(Vec3(normalized.x * player.speed, normalized.y * player.speed, 0.0))
+
+        -- Set the rotation of the player/robot based on the direction it is moving
+        local angle = toDegrees(vec2Angle(normalized))
+        player:setRotation(Quaternion(Vec3(0, 0, 1), angle))
+
+        -- Put the camera of the robot on the right place after the player moved, based on the ROBOT_CAMERA_OFFSET
+        local offset = vec2Rotate(ROBOT_CAMERA_OFFSET, angle)
+        player.robotCamera:setPosition(player:getPosition() + offset)
+    else 
+        player.pc:getRigidBody():setLinearVelocity(Vec3(0.0, 0.0, 0.0))
+    end
+
+    -- Make the camera of the robot rotation slowly
+    updateRobotCameraRotation(player:getRotation())
 	
 	-- space press
 	if(bulletCooldown ~= 0.2) then
@@ -50,17 +93,6 @@ player.update = function (guid, deltaTime)
 		bulletCooldown = bulletCooldown - deltaTime
     end
 
-    -- If a direction is set, walk 
-    if (direction.x ~= 0 or direction.y ~= 0) then
-        -- Normalize the direction for when two buttons are pressed at the same time
-        local normalized = direction:normalized()
-        -- Multiply speed by the direction to walk
-        -- player:setPosition(player:getPosition() + Vec3(normalized.x * player.speed, normalized.y * player.speed, normalized.z * player.speed))
-
-        player.pc:getRigidBody():setLinearVelocity(Vec3(normalized.x * player.speed, normalized.y * player.speed, 0.0))
-    else 
-        player.pc:getRigidBody():setLinearVelocity(Vec3(0.0, 0.0, 0.0))
-    end
 end
 player.sc = player:createScriptComponent()
 player.sc:setUpdateFunction(player.update)
