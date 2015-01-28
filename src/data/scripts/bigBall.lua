07bbf7adac70efed1e767824dc58511a30641161
Arco Mul <me@arcomul.nl>
2015-01-28 01:35:41 +0100

    Make smaller balls bounce more neathly from the bigger once

--- src/data/scripts/bigBall.lua
+++ src/data/scripts/bigBall.lua
@@ -23,10 +23,16 @@ function BigBall:update()
 
 	if (lastCollisionData.gotHit) then
 		
+        local velocity = self.go.rb:getLinearVelocity()
+        local ball1Rotation = vec2Rotate(velocity, 90)
+        local ball1RotationNormalized = ball1Rotation:normalized() 
+        local ball2Rotation = vec2Rotate(velocity, -90)
+        local ball2RotationNormalized = ball2Rotation:normalized() 
+
 		local ball1 = objectManager:grab(MediumBall)
-		ball1:setInitialPositionAndMovement(position+Vec3(0,0.76,0), Vec3(0, 2.0, 0.0))
+		ball1:setInitialPositionAndMovement(position + Vec3(ball1RotationNormalized.x * 0.76, ball1RotationNormalized.y * 0.76, 0), Vec3(ball1Rotation.x, ball1Rotation.y, 0.0))
 		local ball2 = objectManager:grab(MediumBall)
-		ball2:setInitialPositionAndMovement(position+Vec3(0,-0.76,0), Vec3(0, -2.0, 0.0))
+		ball2:setInitialPositionAndMovement(position + Vec3(ball2RotationNormalized.x * 0.76, ball2RotationNormalized.y * 0.76, 0), Vec3(ball2Rotation.x, ball2Rotation.y, 0.0))
 		
 		objectManager:put(BigBall, self)
 	end
--- src/data/scripts/mediumBall.lua
+++ src/data/scripts/mediumBall.lua
@@ -24,13 +24,17 @@ function MediumBall:update()
 	if (lastCollisionData.gotHit) then
 		
 		logMessage(self.go:getGuid().."dealing with hitHookshot")
-		
-		
+
+        local velocity = self.go.rb:getLinearVelocity()
+        local ball1Rotation = vec2Rotate(velocity, 90)
+        local ball1RotationNormalized = ball1Rotation:normalized() 
+        local ball2Rotation = vec2Rotate(velocity, -90)
+        local ball2RotationNormalized = ball2Rotation:normalized() 
 		
 		local ball1 = objectManager:grab(SmallBall)
-		ball1:setInitialPositionAndMovement(position+Vec3(0,0.76,0), Vec3(0, 2.0, 0.0))
+		ball1:setInitialPositionAndMovement(position + Vec3(ball1RotationNormalized.x * 0.76, ball1RotationNormalized.y * 0.76, 0), Vec3(ball1Rotation.x, ball1Rotation.y, 0.0))
 		local ball2 = objectManager:grab(SmallBall)
-		ball2:setInitialPositionAndMovement(position+Vec3(0,-0.76,0), Vec3(0, -2.0, 0.0))
+		ball2:setInitialPositionAndMovement(position + Vec3(ball2RotationNormalized.x * 0.76, ball2RotationNormalized.y * 0.76, 0), Vec3(ball2Rotation.x, ball2Rotation.y, 0.0))
 	
 		objectManager:put(MediumBall, self)
 	end
